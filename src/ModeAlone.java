dc8ae4e65c4a953c89df5a949d61c42897abd18f
xoxj618 <remi.couturier-prestataire@labanquepostale.fr>
2019-08-22 11:35:53 +0200

    feat: add semaphore

--- src/ModeAlone.java
+++ src/ModeAlone.java
@@ -1,52 +1,54 @@
+import java.util.concurrent.Semaphore;
+
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.RaspiPin;
 
 public class ModeAlone {
 
     private GpioControler gpioControler;
-    private Thread        mainThread;
-    private Thread        leftThread;
-    private Thread        rightThread;
-    private boolean       isRunning = false;
+    private Thread mainThread;
+    private Thread leftThread;
+    private Thread rightThread;
+    private boolean isRunning = false;
 
     private PiJavaUltrasonic leftCapteur;
     private Pin leftEchoPin = RaspiPin.GPIO_05;
     private Pin leftTrigPin = RaspiPin.GPIO_04;
 
-
     private PiJavaUltrasonic rightCapteur;
     private Pin rightEchoPin = RaspiPin.GPIO_10;
     private Pin rightTrigPin = RaspiPin.GPIO_06;
-
 
     public int leftDistance;
     public int rightDistance;
 
     private class CapteurThread implements Runnable {
 
+        private Semaphore semaphore;
         private PiJavaUltrasonic piJavaUltrasonic;
 
-        public CapteurThread(PiJavaUltrasonic piJavaUltrasonic) {
+        public CapteurThread(PiJavaUltrasonic piJavaUltrasonic, Semaphore semaphore) {
             this.piJavaUltrasonic = piJavaUltrasonic;
+            this.semaphore = semaphore;
         }
 
         @Override
         public void run() {
             while (isRunning) {
+                semaphore.release();
                 try {
                     int distance = piJavaUltrasonic.getDistance();
-                    if (piJavaUltrasonic.name.contains("LEFT")){
+                    if (piJavaUltrasonic.name.contains("LEFT")) {
                         leftDistance = distance;
                     }
-                    if (piJavaUltrasonic.name.contains("RIGHT")){
+                    if (piJavaUltrasonic.name.contains("RIGHT")) {
                         rightDistance = distance;
                     }
-                    // Wait 1 sec
-                    Thread.sleep(1000);
                 } catch (Exception e) {
                     Thread.currentThread().interrupt();
                     System.out.println("--> leftCapteurThread Error : " + e);
                 }
+                semaphore.acquire();
             }
         }
     }
@@ -57,9 +59,13 @@ public class ModeAlone {
     private class AloneModeThread implements Runnable {
 
         public AloneModeThread() {
+
+            // Creat Semaphore
+            Semaphore semaphore = new Semaphore(2, true);
+
             // Start getDistance
-            leftThread = new Thread(new CapteurThread(leftCapteur));
-            rightThread = new Thread(new CapteurThread(rightCapteur));
+            leftThread = new Thread(new CapteurThread(leftCapteur, semaphore));
+            rightThread = new Thread(new CapteurThread(rightCapteur, semaphore));
             leftThread.start();
             rightThread.start();
         }
@@ -69,20 +75,21 @@ public class ModeAlone {
             while (isRunning) {
                 try {
                     Thread.sleep(500);
-                    System.out.println("--> Distance : L = " + leftDistance + " cm / R = " + rightDistance + " cm");
+                    // System.out.println("--> Distance : L = " + leftDistance + " cm / R = " +
+                    // rightDistance + " cm");
                     float turnRobot = checkDirection(leftDistance, rightDistance);
                     if (leftDistance >= 100) {
-                        gpioControler.leftMotor.controlMotor(100,1);
+                        gpioControler.leftMotor.controlMotor(100, 1);
                     } else if (leftDistance <= 10) {
                         gpioControler.leftMotor.controlMotor(0, 1);
                     } else if (leftDistance <= 20) {
                         gpioControler.leftMotor.controlMotor(20, 1);
                     } else {
-                        gpioControler.leftMotor.controlMotor(leftDistance,1);
+                        gpioControler.leftMotor.controlMotor(leftDistance, 1);
                     }
                 } catch (Exception e) {
                     Thread.currentThread().interrupt();
-                    System.out.println("!! AloneModeThread Error : " + e);
+                    System.out.println("--> AloneModeThread Error : " + e);
                 }
             }
         }
@@ -97,8 +104,10 @@ public class ModeAlone {
         // Create gpio controller for motor
         try {
             this.gpioControler = gpioControler;
-            leftCapteur = new PiJavaUltrasonic(leftEchoPin.getAddress(), leftTrigPin.getAddress(), 1000, 23529411, "LEFT");
-            rightCapteur = new PiJavaUltrasonic(rightEchoPin.getAddress(), rightTrigPin.getAddress(), 1000, 23529411, "RIGHT");
+            leftCapteur = new PiJavaUltrasonic(leftEchoPin.getAddress(), leftTrigPin.getAddress(), 1000, 23529411,
+                    "LEFT");
+            rightCapteur = new PiJavaUltrasonic(rightEchoPin.getAddress(), rightTrigPin.getAddress(), 1000, 23529411,
+                    "RIGHT");
         } catch (Exception ex) {
             System.out.println("--> ModeAlone Error : " + ex);
         }
@@ -138,18 +147,18 @@ public class ModeAlone {
      * @return
      */
     private float checkDirection(int leftDist, int rightDist) {
-            if (leftDist > rightDist) {
-                // Turn Left
-                System.out.println("--> Turn Left  : <-O");
-                return -1;
-            } else if (leftDist < rightDist) {
-                // Turn Right
-                System.out.println("--> Turn Right :  O->");
-                return 1;
-            } else {
-                // Center
-                System.out.println("--> Go Center  :  |||");
-                return 0;
-            }
+        if (leftDist > rightDist) {
+            // Turn Left
+            System.out.println("--> Turn Left  : <-O");
+            return -1;
+        } else if (leftDist < rightDist) {
+            // Turn Right
+            System.out.println("--> Turn Right :  O->");
+            return 1;
+        } else {
+            // Center
+            System.out.println("--> Go Center  :  |||");
+            return 0;
+        }
     }
 }
